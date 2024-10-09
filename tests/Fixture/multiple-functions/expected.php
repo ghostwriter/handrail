f42fd1a8c25571d4e244fed94874e4b904cc375c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-09 15:17:24 -0500

    Add expected.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/multiple-functions/expected.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+if (!function_exists('exampleFunction')) {
+    function exampleFunction() {
+        // some code
+    }
+}
+
+if (!function_exists('anotherFunction')) {
+    function anotherFunction() {
+        // more code
+    }
+}
