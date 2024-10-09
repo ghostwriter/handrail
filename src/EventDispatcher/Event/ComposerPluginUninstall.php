ddc1a82e4cf5736f4c0f77dee5d52753dce2ef5d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-09 15:17:20 -0500

    Add ComposerPluginUninstall.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/EventDispatcher/Event/ComposerPluginUninstall.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Handrail\EventDispatcher\Event;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+
+final class ComposerPluginUninstall
+{
+    public function __construct(
+        private Composer $composer,
+        private IOInterface $io
+    ) {
+    }
+
+    public function getComposer(): Composer
+    {
+        return $this->composer;
+    }
+
+    public function getIO(): IOInterface
+    {
+        return $this->io;
+    }
+}
