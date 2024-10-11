2029b810c84c57dccec18f42bb5668e4761ffce6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-11 09:49:53 -0500

    Add Path.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Value/Path.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Handrail\Value;
+
+use Override;
+
+final class Path implements PathInterface
+{
+    /**
+     * @var array<non-empty-string,self>
+     */
+    private static array $instances = [];
+
+    /**
+     * @param non-empty-string $path
+     */
+    private function __construct(
+        private readonly string $path,
+    ) {
+    }
+
+    /**
+     * @param non-empty-string $path
+     */
+    #[Override]
+    public static function new(string $path): self
+    {
+        return self::$instances[$path] ??= new self($path);
+    }
+
+    public function __destruct()
+    {
+        unset(self::$instances[$this->path]);
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    #[Override]
+    public function __toString(): string
+    {
+        return $this->path;
+    }
+}
