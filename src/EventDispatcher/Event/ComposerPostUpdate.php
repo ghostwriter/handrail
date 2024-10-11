e6ca59a01bced58f08cca271f8cd1a5381fb344d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-11 09:49:51 -0500

    Update ComposerPostUpdate.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/EventDispatcher/Event/ComposerPostUpdate.php
+++ src/EventDispatcher/Event/ComposerPostUpdate.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Handrail\EventDispatcher\Event;
 
-use Composer\Composer;
-use Composer\IO\IOInterface;
 use Composer\Script\Event;
 
-final class ComposerPostUpdate
+final readonly class ComposerPostUpdate
 {
     public function __construct(
         private Event $event,
@@ -18,15 +16,5 @@ final class ComposerPostUpdate
     public function event(): Event
     {
         return $this->event;
-    }
-
-    public function getComposer(): Composer
-    {
-        return $this->event->getComposer();
-    }
-
-    public function getIO(): IOInterface
-    {
-        return $this->event->getIO();
     }
 }
