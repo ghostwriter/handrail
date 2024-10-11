f7d48960c1d863080cb87a33d6167cd045b7f80e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-11 09:49:52 -0500

    Update ModifierInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Modifier/ModifierInterface.php
+++ src/Modifier/ModifierInterface.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Handrail\Modifier;
 
-use Ghostwriter\Handrail\File\IncludedFileInterface;
-use Ghostwriter\Handrail\File\ModifiedFileInterface;
+use Ghostwriter\Handrail\Value\File\ModifiedFileInterface;
+use Ghostwriter\Handrail\Value\File\OriginalFileInterface;
 
 interface ModifierInterface
 {
-    public function modify(IncludedFileInterface $file): ModifiedFileInterface;
+    public function modify(OriginalFileInterface $originalFile): ModifiedFileInterface;
 }
