852bdd9f67dacaf2e1e8bbaf3b029b967e2e41ff
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-11 09:49:50 -0500

    Update ServiceProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Container/ServiceProvider.php
+++ src/Container/ServiceProvider.php
@@ -15,16 +15,20 @@ use Ghostwriter\Filesystem\Interface\FilesystemInterface;
 use Ghostwriter\Handrail\Container\Factory\ListenerProviderFactory;
 use Ghostwriter\Handrail\Handrail;
 use Ghostwriter\Handrail\HandrailInterface;
+use Ghostwriter\Json\Interface\JsonInterface;
+use Ghostwriter\Json\Json;
+use Override;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 
-final class ServiceProvider implements ServiceProviderInterface
+final readonly class ServiceProvider implements ServiceProviderInterface
 {
     /**
      * Registers a service on the given container.
      */
+    #[Override]
     public function __invoke(ContainerInterface $container): void
     {
         $container->alias(ArgvInput::class, InputInterface::class);
@@ -33,6 +37,7 @@ final class ServiceProvider implements ServiceProviderInterface
         $container->alias(Filesystem::class, FilesystemInterface::class);
         $container->alias(Handrail::class, HandrailInterface::class);
         $container->alias(ListenerProvider::class, ListenerProviderInterface::class);
+        $container->alias(Json::class, JsonInterface::class);
         $container->factory(ListenerProvider::class, ListenerProviderFactory::class);
     }
 }
