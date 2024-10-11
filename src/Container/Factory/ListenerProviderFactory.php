c526dfeb251261a7d85e2c0c800a4d7a6182b0a6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-11 09:49:50 -0500

    Update ListenerProviderFactory.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Container/Factory/ListenerProviderFactory.php
+++ src/Container/Factory/ListenerProviderFactory.php
@@ -7,31 +7,24 @@ namespace Ghostwriter\Handrail\Container\Factory;
 use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\Container\Interface\FactoryInterface;
 use Ghostwriter\EventDispatcher\ListenerProvider;
-use Ghostwriter\Handrail\EventDispatcher\Event\ComposerPluginActivate;
-use Ghostwriter\Handrail\EventDispatcher\Event\ComposerPluginDeactivate;
-use Ghostwriter\Handrail\EventDispatcher\Event\ComposerPluginUninstall;
 use Ghostwriter\Handrail\EventDispatcher\Event\ComposerPostInstall;
 use Ghostwriter\Handrail\EventDispatcher\Event\ComposerPostUpdate;
-use Ghostwriter\Handrail\EventDispatcher\Listener\ComposerPluginActivateListener;
-use Ghostwriter\Handrail\EventDispatcher\Listener\ComposerPluginDeactivateListener;
-use Ghostwriter\Handrail\EventDispatcher\Listener\ComposerPluginUninstallListener;
 use Ghostwriter\Handrail\EventDispatcher\Listener\ComposerPostInstallListener;
 use Ghostwriter\Handrail\EventDispatcher\Listener\ComposerPostUpdateListener;
+use Override;
 
 /**
  * @implements FactoryInterface<ListenerProvider>
  */
-final class ListenerProviderFactory implements FactoryInterface
+final readonly class ListenerProviderFactory implements FactoryInterface
 {
     /**
      * @return ListenerProvider
      */
+    #[Override]
     public function __invoke(ContainerInterface $container): object
     {
         $listenerProvider = new ListenerProvider($container);
-        $listenerProvider->bind(ComposerPluginActivate::class, ComposerPluginActivateListener::class);
-        $listenerProvider->bind(ComposerPluginDeactivate::class, ComposerPluginDeactivateListener::class);
-        $listenerProvider->bind(ComposerPluginUninstall::class, ComposerPluginUninstallListener::class);
 
         $listenerProvider->bind(ComposerPostInstall::class, ComposerPostInstallListener::class);
         $listenerProvider->bind(ComposerPostUpdate::class, ComposerPostUpdateListener::class);
