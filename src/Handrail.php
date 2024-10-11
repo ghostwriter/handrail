246c5738760603d5ec08d8734402b45539df820a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-11 09:49:52 -0500

    Update Handrail.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Handrail.php
+++ src/Handrail.php
@@ -4,45 +4,42 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Handrail;
 
-use Composer\Composer;
-use Composer\IO\IOInterface;
-use Ghostwriter\Container\Attribute\Inject;
 use Ghostwriter\Container\Container;
-use Ghostwriter\Filesystem\Filesystem;
+use Ghostwriter\EventDispatcher\Interface\EventDispatcherInterface;
 use Ghostwriter\Filesystem\Interface\FilesystemInterface;
 use Ghostwriter\Handrail\Container\ServiceProvider;
 use Ghostwriter\Handrail\Exception\ShouldNotHappenException;
-use Ghostwriter\Handrail\File\IncludedFileInterface;
-use Ghostwriter\Handrail\File\ModifiedFileInterface;
-use Ghostwriter\Handrail\Modifier\FileModifier;
-use Ghostwriter\Handrail\Paths\ExcludePaths;
-use Ghostwriter\Handrail\Paths\IncludePaths;
+use Ghostwriter\Handrail\Modifier\FunctionDeclarationModifier;
+use Ghostwriter\Handrail\Value\File\OriginalFileInterface;
+use Ghostwriter\Handrail\Value\Path;
+use Ghostwriter\Handrail\Value\Paths;
 use Override;
-use SplFileInfo;
 use Throwable;
-
-use const PHP_EOL;
-use const STDOUT;
 
 /** @see HandrailTest */
 final readonly class Handrail implements HandrailInterface
 {
+    public const string PACKAGE_NAME = 'ghostwriter/handrail';
+
+    public const string OPTION_FILES = 'files';
+
+    public const string OPTION_EXCLUDE = 'exclude';
+
+    public const string OPTION_INCLUDE = 'include';
+
+    public const string OPTION_DISABLE = 'disable';
+
+    public const string EXTRA = 'extra';
+
     public function __construct(
-        private IncludePaths $includePaths,
-        private ExcludePaths $excludePaths,
-        #[Inject(Filesystem::class)]
+        private Paths $paths,
         private FilesystemInterface $filesystem,
-        private FileModifier $fileModifier,
+        private FunctionDeclarationModifier $functionDeclarationModifier,
+        private EventDispatcherInterface $eventDispatcher,
     ) {
     }
 
-    /**
-     * @param list<string> $includePaths
-     * @param list<string> $excludePaths
-     *
-     * @throws Throwable
-     */
-    public static function new(array $includePaths = [], array $excludePaths = []): self
+    public static function new(): self
     {
         $container = Container::getInstance();
 
@@ -50,126 +47,30 @@ final readonly class Handrail implements HandrailInterface
             $container->provide(ServiceProvider::class);
         }
 
-        $self = $container->get(self::class);
-        $self->exclude(...$excludePaths);
-        $self->include(...$includePaths);
-        return $self;
+        return $container->get(self::class);
     }
 
     /**
-     * @throws Throwable
-     */
-    public function exclude(string ...$paths): self
-    {
-        foreach ($paths as $path) {
-            match (true) {
-                $this->filesystem->isDirectory($path) => $this->excludeDirectory($path),
-                $this->filesystem->isFile($path) => $this->excludeFile($path),
-                default => throw new ShouldNotHappenException(\sprintf('Path is not a file or directory: %s', $path)),
-            };
-        }
-
-        return $this;
-    }
-
-    /**
-     * @throws Throwable
-     */
-    #[Override]
-    public function guard(string $path): void
-    {
-        match (true) {
-            $this->filesystem->isDirectory($path) => $this->guardDirectory($path),
-            $this->filesystem->isFile($path) => $this->guardFile($path),
-            default => throw new ShouldNotHappenException(\sprintf('Path is not a file or directory: %s', $path)),
-        };
-
-        /** @var IncludedFileInterface[] $phpFiles */
-        $phpFiles = \array_diff($this->includePaths->toArray(), $this->excludePaths->toArray());
-
-        foreach ($phpFiles as &$phpFile) {
-            $modifiedPhpFile = $phpFile->modify($this->fileModifier);
-
-            if ($modifiedPhpFile->code() === $phpFile->code()) {
-                continue;
-            }
-
-            if (! $modifiedPhpFile instanceof ModifiedFileInterface) {
-                return;
-            }
-
-            $modifiedPhpFile->save($this->filesystem);
-        }
-    }
-
-    /**
-     * @throws Throwable
-     */
-    public function include(string ...$paths): self
-    {
-        foreach ($paths as $path) {
-            match (true) {
-                $this->filesystem->isDirectory($path) => $this->includeDirectory($path),
-                $this->filesystem->isFile($path) => $this->includeFile($path),
-                default => throw new ShouldNotHappenException(
-                    \sprintf('Path is not a file or directory: %s', $path)
-                ),
-            };
-        }
-
-        return $this;
-    }
-
-    /**
-     * @throws Throwable
-     */
-    private function excludeDirectory(string $path): void
-    {
-        foreach ($this->filesystem->recursiveRegexIterator(
-            $this->filesystem->recursiveDirectoryIterator($path),
-            '#^.+\.php$#i'
-        ) as $splFileInfo) {
-            if (! $splFileInfo instanceof SplFileInfo) {
-                continue;
-            }
-
-            $this->excludeFile($splFileInfo->getPathname());
-        }
-    }
-
-    /**
-     * @param non-empty-string $path
+     * @param non-empty-string ...$files
      *
      * @throws Throwable
      */
-    private function excludeFile(string $path): void
+    #[Override]
+    public function guard(string ...$files): void
     {
-        $this->excludePaths->add(Path::new($path));
-    }
-
-    /**
-     * @throws Throwable
-     */
-    private function guardDirectory(string $directory): void
-    {
-        foreach ($this->filesystem->recursiveRegexIterator(
-            $this->filesystem->recursiveDirectoryIterator($directory),
-            '#^.+\.php$#i'
-        ) as $splFileInfo) {
-            if (! $splFileInfo instanceof SplFileInfo) {
-                continue;
+        foreach ($files as $file) {
+            if (! $this->filesystem->isFile($file)) {
+                throw new ShouldNotHappenException(\sprintf('Path is not a file: %s', $file));
             }
 
-            $path = Path::new($splFileInfo->getPathname());
+            $this->guardFile($file);
+        }
 
-            if (
-                $this->includePaths->contains($path) ||
-                $this->excludePaths->contains($path)
-            ) {
-                continue;
-            }
+        /** @var OriginalFileInterface $includedFile */
+        foreach ($this->paths->toArray() as $includedFile) {
+            $modifiedFile = $includedFile->modify($this->functionDeclarationModifier);
 
-            $this->includePaths->add($path);
+            $modifiedFile->save($this->filesystem);
         }
     }
 
@@ -182,65 +83,10 @@ final readonly class Handrail implements HandrailInterface
     {
         $path = Path::new($path);
 
-        if (
-            $this->includePaths->contains($path) ||
-            $this->excludePaths->contains($path)
-        ) {
+        if ($this->paths->contains($path)) {
             return;
         }
 
-        $this->includePaths->add($path);
-    }
-
-    /**
-     * @throws Throwable
-     */
-    private function includeDirectory(string $directory): void
-    {
-        foreach ($this->filesystem->recursiveRegexIterator(
-            $this->filesystem->recursiveDirectoryIterator($directory),
-            '#^.+\.php$#i'
-        ) as $splFileInfo) {
-            if (! $splFileInfo instanceof SplFileInfo) {
-                continue;
-            }
-
-            $this->includeFile($splFileInfo->getPathname());
-        }
-    }
-
-    /**
-     * @param non-empty-string $path
-     *
-     * @throws Throwable
-     */
-    private function includeFile(string $path): void
-    {
-        $this->includePaths->add(Path::new($path));
-    }
-
-    public static function activate(Composer $composer, IOInterface $io): void
-    {
-        \fwrite(STDOUT, self::class . ' - ' . __METHOD__ . PHP_EOL);
-    }
-
-    public static function deactivate(Composer $composer, IOInterface $io): void
-    {
-        \fwrite(STDOUT, self::class . ' - ' . __METHOD__ . PHP_EOL);
-    }
-
-    public static function postInstall(\Composer\Script\Event $event): void
-    {
-        \fwrite(STDOUT, self::class . ' - ' . $event::class . ' - ' . __METHOD__ . PHP_EOL);
-    }
-
-    public static function postUpdate(\Composer\Script\Event $event): void
-    {
-        \fwrite(STDOUT, self::class . ' - ' . $event::class . ' - ' . __METHOD__ . PHP_EOL);
-    }
-
-    public static function uninstall(Composer $composer, IOInterface $io): void
-    {
-        \fwrite(STDOUT, self::class . ' - ' . __METHOD__ . PHP_EOL);
+        $this->paths->add($path);
     }
 }
